2ae170eb4adab4c2a821377c56dc95b3c0e4e8e0
Alex <sakalinski95@gmail.com>
2023-07-17 21:17:03 +0300

    Add 2 projects

---
+++ project1/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>LAB_2</title>        
+    </head>
+    <body>
+            <form action="" method="POST">
+            <label for="a">Введите a</label><br>
+            <input type ="text" placeholder="1" id="a" name="a"><br>
+            <label for="b">Введите b</label><br>
+            <input type ="text" placeholder="1" id="b" name="b"><br>
+            <label for="h">Введите h</label><br>
+            <input type ="text" placeholder="1" id="h" name="h"><br>
+            <label for="n">Введите n</label><br>
+            <input type ="text" placeholder="1" id="n" name="n"><br>
+            <input type = "submit">
+            </form>                    
+            <table>           
+            <tr>    
+            <th>x</th><th>S(x)</th><th>Y(x)</th><th>|Y(x)-S(x)|</th>   
+            </tr>
+            <?php
+            if(!empty($_POST["a"]) && !empty($_POST["b"]) && !empty($_POST["h"]) && !empty($_POST["n"]))
+            {
+                if(preg_match("|^[\d]*[\.,]?[\d]*$|", $_POST['a']) && preg_match("|^[\d]*[\.,]?[\d]*$|", $_POST['b']) 
+                && preg_match("|^[\d]*[\.,]?[\d]*$|", $_POST['h']) && preg_match("|^[\d]*[\.,]?[\d]*$|", $_POST['n']))
+                {                 
+                $n; $a = 0.1; $b = 1; $h = 0.1; $x; $y; $s; $delta; $k;
+                $a = (float)$_POST["a"]; 
+                $b = (float)$_POST["b"];
+                $h = (float)$_POST["h"];
+                $n = (int)$_POST["n"];                
+                for ($x = $a; $x <= $b; $x += $h)
+	            {
+		            $y = (pow($x, 2) - pow(pi(), 2) / 3)*1 / 4;
+		            $s = 0;
+		            for ($k = 1; $k <= $n; $k++)
+		            {
+			            $s += pow(-1, $k)*cos($k*$x) / pow($k, 2);
+		            }
+		        $delta = abs($y - $s);                
+            ?>	
+                <tr>
+                <td><?= $x?></td>
+                <td><?= $s?></td>
+                <td><?= $y?></td>
+                <td><?= $delta?></td>
+                </tr>       
+            <?
+                }
+            }
+        }
+            ?>            
+        </table> 
+    </body>
+</html>
---
+++ project2/index.php
@@ -0,0 +1,49 @@
+<?php
+$a; $b; $h; $a1; $a2;
+$a = -4;
+$b = 0;
+$h = ($b - $a) / 20;
+$eps = pow(10, -4);
+for ($a; $a <= $b; $a += $h) {
+    $a1 = $a;
+    $a2 = $a + $h;
+    if ((y($a1)*y($a2)) < 0) {
+        iter($a1, $a2, $eps);
+        delen($a1, $a2, $eps);
+    }
+}
+
+function y($x)
+{
+   return pow(sin($x), 2) - $x / 5 - 1;
+}	
+
+
+function iter($a1, $a2, $eps) {
+	$n = 0;
+	$x1 = 0;
+	$x2 = 0;
+	for ($a1; $a1 <= $a2; $a1 += $eps)
+	{
+	    $x1 = $a1;
+		$x2 = $x1 + $eps;
+		++$n;
+		if ((y($x1)*y($x2)) < 0) {
+			echo "Iteration y=0 for x=$a1 iterations $n <br>";
+		}
+	}
+
+}
+
+function delen($a1, $a2, $eps) {
+	$n = 0;
+	while (($a2 - $a1) > $eps) {
+		$a0 = ($a1 + $a2) / 2;
+		if ((y($a0)*y($a2)) > 0) { $a2 = $a0; }
+		if ((y($a0)*y($a1)) > 0) { $a1 = $a0; }
+		++$n;
+		if ((y($a1)*y($a2)) < 0 && ($a2 - $a1) < $eps) 
+        { echo "Delenie y=0 for x=$a1 iterations $n<br>"; }
+	}
+}
+?>
